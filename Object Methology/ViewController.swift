454d86293a75fb8e5406fedba4eb5775dde5ee0d
Timofey Solonin <tim-solonin2@yandex.ru>
2017-09-28 23:26:21 +0300

    Initial commit

---
+++ Object Methology/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Object Methology
+//
+//  Created by Timofey on 9/28/17.
+//  Copyright © 2017 NFO. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
